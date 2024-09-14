d5c22625365bd33bbbd46f2a0d59b245ae1942c1
CuriousR82 <rosajeon82@gmail.com>
2024-09-14 16:00:24 -0400

    login function

--- driver.py
+++ driver.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.service import Service
 class WebAgent:
     def __init__(self):
         # Launch Chrome with specified settings
-        service = Service(executable_path="/usr/bin/chromedriver")
+        service = Service(executable_path="/usr/local/bin/chromedriver")
         options = webdriver.ChromeOptions()
         self.driver = webdriver.Chrome(service=service, options=options)
         
@@ -27,12 +27,27 @@ class WebAgent:
 
         search_url = self.driver.current_url
         print(f"Search completed. Current URL: {search_url}")
-    
+
+    def login_kijiji(self, username, password):
+        self.driver.get("http://kijiji.ca")
+
+        # Click on the Sign In button
+        sign_in_page = self.driver.find_element(By.CSS_SELECTOR, 'button[data-testid="header-sign-in"]')
+        sign_in_page.click()
+        email_address_box = self.driver.find_element(By.ID, 'username')
+        password_box = self.driver.find_element(By.ID, 'password')
+        sign_in_button = self.driver.find_element(By.CSS_SELECTOR, 'button#login-submit')
+        email_address_box.send_keys(username)
+        password_box.send_keys(password)
+        sign_in_button.click()
+
+
     def close(self):
         # Wait for 5 seconds before closing the browser
-        time.sleep(5)
+        time.sleep(10)
         self.driver.quit()
 
 web_agent = WebAgent()
-web_agent.search_kijiji("rtx-4090")
+# web_agent.search_kijiji("rtx-4090")
+web_agent.login_kijiji("rosajeon82@gmail.com", "@PriceWhisper24")
 web_agent.close()
