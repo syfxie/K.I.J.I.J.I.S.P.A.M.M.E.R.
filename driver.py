2615b187b6f8a952b3677c17be630f686fb26641
CuriousR82 <rosajeon82@gmail.com>
2024-09-14 22:28:04 -0400

    GUI launch page

--- driver.py
+++ driver.py
@@ -1,5 +1,6 @@
 from WebAgent import WebAgent
 from Scraper import KijijiScraper
+from gui import GUI
 
 web_agent = WebAgent()
 search_url = web_agent.search_kijiji("rtx-4090")
@@ -11,3 +12,6 @@ print(df)
 web_agent.close()
 
 df.to_csv("data.csv", encoding="utf-8", index=False)
+
+gui = GUI()
+gui.run()
