714c69a159d14bc167c9e5b1fed61238e5b09c03
sophiexie <sophiexie118@gmail.com>
2024-09-14 23:53:31 -0400

    Detect and response to new messages

--- main.py
+++ main.py
@@ -1,45 +1,31 @@
-# from WebAgent import *
-# from Scraper import *
 import pandas as pd
 import time
+
 from orchestrator import Orchestrator
+from agent import MessagingAgent, FilteringAgent, DealClosingAgent
+
 
 if __name__ == "__main__":
     orchestrator = Orchestrator(count=2)
+    agent_names = orchestrator.get_agent_names()
+    # negotiator = DealClosingAgent()
+
+    # Dict : DataFrame
+    inital_convos = orchestrator.get_data()
+    while True:
+        is_update = orchestrator.check_for_update()
+        if is_update: 
+            print("THERE IS AN UPDATE -----")
+            messages = orchestrator.get_data()
+            print(messages)
+            agent = MessagingAgent()
+            next_messages = agent.gen_next_msg(messages)
+            print(next_messages)
+
+        time.sleep(10)
+
+
+
     time.sleep(10)
     orchestrator.close()
 
-
-    # web_agent = WebAgent()
-    # scraper = KijijiScraper()
-
-    # search_url = web_agent.search_kijiji("rtx-4090")
-    
-    # df = scraper.scrape_search_result(search_url)
-
-    # web_agent.close()
-    # df.to_excel("data.xlsx", engine="xlsxwriter", index=False)
-
-    # web_agent = WebAgent()
-    # # search_url = web_agent.search_kijiji("rtx-4090")
-
-    # # scraper = KijijiScraper()
-    # #
-    # # df = scraper.scrape(search_url)
-    # # print(df)
-    # df = pd.DataFrame()
-
-    # web_agent.login_kijiji(
-    #     username="",
-    #     password="",
-    # )
-
-    # messages = web_agent.parse_messages()
-
-    # # Pause the script and keep the browser open
-    # input("Press Enter to close the browser...")
-
-    # # Close the browser after the user presses Enter
-    # web_agent.close()
-
-    # messages.to_excel("data.xlsx", engine="xlsxwriter", index=False)
--- orchestrator.py
+++ orchestrator.py
@@ -21,21 +21,15 @@ class Orchestrator:
         }
 
         for i in range(count):
-            self.web_agents[i].set_name(f"Agent {i}")
+            self.web_agents[i].set_name(f"Agent {i + 1}")
             self.web_agents[i].set_username(ACCOUNTS[i]["username"])
             self.web_agents[i].set_password(ACCOUNTS[i]["password"])
             self.web_agents[i].login_kijiji()
             self.web_agents[i].goto_first_convo()
             self.dfs[self.web_agents[i]] = self.web_agents[i]._parse_convo()
-
-        self.get_initial_convos()
-
-    def get_initial_convos(self):
-        res = {}
-        for agent in self.web_agents:
-            res[agent.name] = self.dfs[agent]
-
-        return res
+    
+    def get_agent_names(self):
+        return [agent.name for agent in self.web_agents]
 
     def check_for_update(self):
         for agent in self.web_agents:
@@ -43,12 +37,18 @@ class Orchestrator:
             if len(self.dfs[agent]) < len(new_df):
                 self.dfs[agent] = new_df
                 print(f"Agent {agent.name} has a new message!")
-                self.send_update(agent.name, new_df)
+                return True
+        
+        return False
 
-    def send_update(self, agent_name, new_df):
-        last_row_dict = new_df.iloc[-1].to_dict()
-        return (agent_name, last_row_dict)
+    def get_data(self):
+        res = {}
+        for agent in self.web_agents:
+            res[agent.name] = [tuple(row) for row in self.dfs[agent].values]
+        
+        return res
 
     def close(self):
         for agent in self.web_agents:
             agent.close()
+            3
